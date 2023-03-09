977a47e2c4aa1c3c89951bea2b54d64a2e9e01b0
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-03-09 21:24:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Work Sprint Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title = Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg="black", bg=YELLOW)
+title.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+ticks = Label(text="✔", fg="green", font=(FONT_NAME, 15, "bold"), bg=YELLOW)
+ticks.grid(column=1, row=3)
+
+
+
+window.mainloop()
+
+
